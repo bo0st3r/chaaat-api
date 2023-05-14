940fc7681b6eccd8e2ce29525827bf0ae444a852
bastien <bastien@docmagic.com>
2023-05-13 21:03:26 -0400

    feat: add health checks

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { OpenAPIModule } from './openapi/openapi.module';
 import { ChatService } from './chat/chat.service';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), OpenAPIModule],
+  imports: [ConfigModule.forRoot(), OpenAPIModule, HealthModule],
   controllers: [AppController],
   providers: [ChatService],
 })
